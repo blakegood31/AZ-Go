503ea737c21578751593e1376a062aec80fa075b
Edward Chor <13307130149@fudan.edu.cn>
2018-04-10 19:04:17 -0400

    change iters to 1000

--- GoMain.py
+++ GoMain.py
@@ -4,7 +4,7 @@ from go.pytorch.NNet import NNetWrapper as nn
 from utils import *
 
 args = dotdict({
-    'numIters': 100,
+    'numIters': 1000,
     'numEps': 100,
     'tempThreshold': 15,
     'updateThreshold': 0.6,
