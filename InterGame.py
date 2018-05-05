e8ed7e6c4a37857db8665982ff2dac05f6544cd5
edwardchor <zc1213@deeplearning.c.adroit-petal-193002.internal>
2018-05-05 02:54:22 +0000

    socket test

--- InterGame.py
+++ InterGame.py
@@ -23,9 +23,10 @@ class InterGame(object):
         self.n=self.game.getBoardSize()[0]
         self.players=[self.AlphaPlay,None,self.HumanPlay]
         self.curPlayer=1
+        self.gameStatus=0
         if NetType=='ResNet':
             self.AlphaNet=nn(self.game,t='RES')
-            self.AlphaNet.load_checkpoint('./HistoryLog/Go/R_MCTS_SimModified_checkpoint/{}/'.format(BoardSize),'best.pth.tar')
+            self.AlphaNet.load_checkpoint('./HistoryLog/Go/R_Ver2_checkpoint/{}/'.format(BoardSize),'RVer2.best.pth.tar')
             self.AlphaArgs = dotdict({'numMCTSSims': 2000, 'cpuct':17.3})
             self.AlphaMCTS = MCTS(self.game, self.AlphaNet,self.AlphaArgs)
             self.Alpha= lambda x: np.argmax(self.AlphaMCTS.getActionProb(x, temp=0))
@@ -38,9 +39,20 @@ class InterGame(object):
 
     def initialize(self):
         self.board=self.game.getInitBoard()
-
+    def judgeGame(self):
+        self.gameStatus=self.game.getGameEnded(self.board,self.curPlayer)
+        if self.gameStatus==-1:
+            print("player 1 lost.")
+            return -1
+        elif self.gameStatus==1:
+            print("player 1 won.")
+            return 1
+        else:
+            print("game continues.")
+            return 0
     def AlphaPlay(self,*move):
-
+        assert(self.judgeGame()==0)
+        
         action = self.Alpha(self.game.getCanonicalForm(self.board,self.curPlayer ))
         valids = self.game.getValidMoves(self.game.getCanonicalForm(self.board, self.curPlayer),1)
 
@@ -52,10 +64,15 @@ class InterGame(object):
         return (int(action / self.n), action % self.n)
 
     def HumanPlay(self,move):
+        assert(self.judgeGame()==0)
         x,y = [int(x) for x in move]
+        valids = self.game.getValidMoves(self.game.getCanonicalForm(self.board, self.curPlayer),1)
         action= self.game.n * x + y if x!= -1 else self.game.n ** 2
+        if valids[action]==0:
+            print("Invalid Move!")
+            return
         self.board, self.curPlayer = self.game.getNextState(self.board, self.curPlayer, action)
         return 
+    
     def showBoard(self):
         display(self.board)
-        
